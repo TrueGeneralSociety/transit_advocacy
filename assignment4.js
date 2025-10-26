6505b413a92ac28ab59a03960d7660d9fb333c84
TrueGeneralSociety <munwarsamien@gmail.com>
2025-10-25 18:01:46 -0700

    Small change

--- assignment4.js
+++ assignment4.js
@@ -78,5 +78,5 @@ app.use(function (req, res, next) {
 // RUN SERVER
 let port = 8000;
 app.listen(port, function () {
-    console.log("Example app listening on port " + port + "!");
+    console.log("Assignment site listening on port " + port + "!");
 });
