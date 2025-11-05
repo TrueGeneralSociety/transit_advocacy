adf7d9bc2d8c0077fff119f88eedf52f61623385
Samien Munwar <munwarsamien@gmail.com>
2025-11-05 12:20:29 -0800

    Added buttons

--- public/js/client.js
+++ public/js/client.js
@@ -1,2 +1,31 @@
+console.log("Client script loaded.");
 
-console.log("client.js loaded.");
+//DOM elements
+const readCarBtn = document.getElementById("read_car");
+const readBikeBtn = document.getElementById("read_bike");
+const readWalkBtn = document.getElementById("read_walk");
+const readBusBtn = document.getElementById("read_bus");
+const readTramBtn = document.getElementById("read_tram");
+const readMetroBtn = document.getElementById("read_metro");
+const readTrainBtn = document.getElementById("read_train");
+
+// a function declaration inside of a callback ... which takes a callback function :O
+function ajaxGET(url, callback) {
+  const xhr = new XMLHttpRequest();
+
+  //console.log("xhr", xhr);
+  xhr.onload = function () {
+    value = this.responseText;
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      //console.log('responseText:' + xhr.responseText);
+
+      // callback function
+      value = this.responseText;
+      callback(this.responseText);
+    } else {
+      console.log(this.status);
+    }
+  };
+  xhr.open("GET", url); // localhost:8000/weekdays?format=html
+  xhr.send();
+}
