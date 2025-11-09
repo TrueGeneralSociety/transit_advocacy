496f6f8f308dbe0fd314331b8dbea97bdecaeaea
Samien Munwar <munwarsamien@gmail.com>
2025-11-08 18:12:09 -0800

    handlers added

--- public/js/client.js
+++ public/js/client.js
@@ -48,3 +48,117 @@ readCarBtn.addEventListener("click", function () {
     }
   });
 });
+
+let tram_active = false;
+readTramBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/tram.html", function (response) {
+    //read tram.html fragment into a variable
+    const tramElem = document.getElementById("read_more_tram");
+    if (tramElem) {
+      if (tram_active) {
+        tramElem.innerHTML = "";
+        readTramBtn.innerHTML = "Read More";
+        tram_active = false;
+      } else {
+        readTramBtn.innerHTML = "Read Less";
+        tramElem.innerHTML = response;
+        tram_active = true;
+      }
+    }
+  });
+});
+
+let bike_active = false;
+readBikeBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/bike.html", function (response) {
+    //read bike.html fragment into a variable
+    const bikeElem = document.getElementById("read_more_bike");
+    if (bikeElem) {
+      if (bike_active) {
+        bikeElem.innerHTML = "";
+        readBikeBtn.innerHTML = "Read More";
+        bike_active = false;
+      } else {
+        readBikeBtn.innerHTML = "Read Less";
+        bikeElem.innerHTML = response;
+        bike_active = true;
+      }
+    }
+  });
+});
+
+let walk_active = false;
+readWalkBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/walk.html", function (response) {
+    //read walk.html fragment into a variable
+    const walkElem = document.getElementById("read_more_walk");
+    if (walkElem) {
+      if (walk_active) {
+        walkElem.innerHTML = "";
+        readWalkBtn.innerHTML = "Read More";
+        walk_active = false;
+      } else {
+        readWalkBtn.innerHTML = "Read Less";
+        walkElem.innerHTML = response;
+        walk_active = true;
+      }
+    }
+  });
+});
+
+let bus_active = false;
+readBusBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/bus.html", function (response) {
+    //read bus.html fragment into a variable
+    const busElem = document.getElementById("read_more_bus");
+    if (busElem) {
+      if (bus_active) {
+        busElem.innerHTML = "";
+        readBusBtn.innerHTML = "Read More";
+        bus_active = false;
+      } else {
+        readBusBtn.innerHTML = "Read Less";
+        busElem.innerHTML = response;
+        bus_active = true;
+      }
+    }
+  });
+});
+
+let metro_active = false;
+readMetroBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/metro.html", function (response) {
+    //read metro.html fragment into a variable
+    const metroElem = document.getElementById("read_more_metro");
+    if (metroElem) {
+      if (metro_active) {
+        metroElem.innerHTML = "";
+        readMetroBtn.innerHTML = "Read More";
+        metro_active = false;
+      } else {
+        readMetroBtn.innerHTML = "Read Less";
+        metroElem.innerHTML = response;
+        metro_active = true;
+      }
+    }
+  });
+});
+
+let train_active = false;
+readTrainBtn.addEventListener("click", function () {
+  ajaxGET("/data/fragments/train.html", function (response) {
+    //read train.html fragment into a variable
+    const trainElem = document.getElementById("read_more_train");
+    if (trainElem) {
+      if (train_active) {
+        trainElem.innerHTML = "";
+        readTrainBtn.innerHTML = "Read More";
+        train_active = false;
+      } else {
+        readTrainBtn.innerHTML = "Read Less";
+        trainElem.innerHTML = response;
+        train_active = true;
+      }
+    }
+  });
+});
